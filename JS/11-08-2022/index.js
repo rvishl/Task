e7d9e696ddcd665f20a9aa4b93289051c6228129
vishal Raj <rvishl05@gmail.com>
2022-11-06 00:29:27 +0530

    Assignment

--- JS/11-08-2022/index.js
+++ JS/11-08-2022/index.js
@@ -2,11 +2,8 @@
 const keys_obj = Object.keys(users);
 keys_obj.forEach(e => {
     // console.log(e);
-    // console.log(users[e].skills);
-    const check = users[e].skills.filter(ele => {
-        if((ele === "React") && (ele === "Node") && (ele === "MongoDB") && (ele === "Express"))
-            // return true;
-    });
-    console.log(check);
+    console.log(users[e]);
+        
+    
 });
    
